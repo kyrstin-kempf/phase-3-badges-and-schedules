aa3be588339ba97a3e5dc490ec9ec080e681619b
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-06-13 21:32:07 -0400

    commit tree b6216bd886aa663e19d709a148fe3bcd834182c8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+   array.map do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_number|
+        "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(speakers).each do |room_number|
+        puts room_number
+    end
+end
